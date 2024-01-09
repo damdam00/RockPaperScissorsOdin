8b31fe59beb21c52e61000e4020242ec4fe5f83d
damdam00 <mikiubunt@gmail.com>
2024-01-09 22:54:23 +0100

    Added function for checking winner, initializing round and getting player selection from prompt

--- script1.js
+++ script1.js
@@ -1,5 +1,5 @@
 
-const options = ["Rock", "Paper", "Scissors"];
+const options = ["rock", "paper", "scissors"];
 
 function getComputerChoice()
 {
@@ -8,9 +8,35 @@ function getComputerChoice()
     console.log(choice);
 }
 
+function checkWinner(playerSelection, computerSelection)
+{
+    if(playerSelection == computerSelection)
+    {
+        return "Tie";
+    }
+    else if((playerSelection == "rock" && computerSelection == "scissors") ||
+    (playerSelection == "paper" && computerSelection == "rock") || 
+    (playerSelection == "scissors" && computerSelection == "paper"))
+    {
+        return "Player";
+    }
+    else
+    {
+        return "Computer";
+    }
+}
+
 function playRound(playerSelection, computerSelection)
 {
-    
+    const result = checkWinner(playerSelection, computerSelection);
+    if(result == "Player")
+    {
+        return `Player wins, ${playerSelection} beats ${computerSelection}`
+    }
+    else if(result == "Computer")
+    {
+        return `Computer wins, ${computerSelection} beats ${playerSelection}`
+    }
 }
 
 getComputerChoice();
