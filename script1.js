163cb745051086e91136290033f2e33bc2215eda
damdam00 <mikiubunt@gmail.com>
2024-01-09 23:15:45 +0100

    Finished project for now, added final function so start whole game and corrected some minor errors

--- script1.js
+++ script1.js
@@ -4,8 +4,26 @@ const options = ["rock", "paper", "scissors"];
 function getComputerChoice()
 {
     const choice = options[Math.floor(Math.random() * options.length)];
+    return choice;
+}
 
-    console.log(choice);
+function getPlayerChoice()
+{
+    let inputvalid = false;
+    while(inputvalid == false)
+    {
+        const choice = prompt("Rock, paper, scissors?");
+        if(choice == null)
+        {
+            continue;
+        }
+        const choiceLowerCase = choice.toLowerCase();
+        if(options.includes(choiceLowerCase))
+        {
+            inputvalid = true;
+            return choiceLowerCase;
+        }
+    }
 }
 
 function checkWinner(playerSelection, computerSelection)
@@ -31,12 +49,50 @@ function playRound(playerSelection, computerSelection)
     const result = checkWinner(playerSelection, computerSelection);
     if(result == "Player")
     {
-        return `Player wins, ${playerSelection} beats ${computerSelection}`
+        return `Player wins, ${playerSelection} beats ${computerSelection}`;
     }
     else if(result == "Computer")
     {
-        return `Computer wins, ${computerSelection} beats ${playerSelection}`
+        return `Computer wins, ${computerSelection} beats ${playerSelection}`;
+    }
+    else{
+        return "It is tie";
     }
 }
 
-getComputerChoice();
+function game()
+{
+    let scorePlayer = 0;
+    let scoreComputer = 0;
+    
+    for(let i = 0; i<5; i++)
+    {
+        const player = getPlayerChoice();
+        const computer = getComputerChoice();
+        console.log(playRound(player, computer));
+
+        if(checkWinner(player, computer) == "Player")
+        {
+            scorePlayer++;
+        }
+        else if(checkWinner(player, computer) == "Computer")
+        {
+            scoreComputer++;
+        }
+    }
+
+    if(scorePlayer == scoreComputer)
+    {
+        console.log("No one wins!");
+    }
+    else if(scorePlayer > scoreComputer)
+    {
+        console.log("Player wins!");
+    }
+    else
+    {
+        console.log("Computer wins!");
+    }
+}
+
+game();
